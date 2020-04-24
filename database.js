353eac785f00a51caa1cf2592d59f37aab965bff
Behan Remoshan BENET RUBAN <b.remoshan@gmail.com>
2020-04-25 01:45:08 +0200

    Update function of database management

--- database.js
+++ database.js
@@ -14,8 +14,27 @@ class Database {
   createTable = () => {
     console.log("create database table user");
     this.db.run("CREATE TABLE IF NOT EXISTS user(id INTEGER PRIMARY KEY, first_name TEXT, last_name TEXT, date_of_birth TEXT,  place_of_birth TEXT, code_postal TEXT, city TEXT, address TEXT)",  (err) => {if (err){
-      console.log("Failed to create table")
+      console.log("Failed to create table");
     }});
+  }
+  
+  insertUser = (user_id) => {
+    this.db.run('INSERT INTO user (id) VALUES (?)', user_id, (err) => {
+      if (err) {
+        console.log("User already exist");
+      }
+    });
+  }
+
+  updateUser = (user_id, value, field_name) => {
+    sql = `UPDATE user SET ${field_name} = ? WHERE id = ?`;
+    this.db.run(sql, (value, user_id), (err) => {
+      if (err) {
+        console.log("Update failed");
+        return false;
+      }
+      return true;
+    }); 
   }
 }
  
